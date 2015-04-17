3e2323bac8943cec188b6706822ac42f32f27ebe
Ross Illingworth <RossL@devinfotech.co.uk>
2015-04-17 16:04:27 +0100

    added loads to get plantuml working... encode/deflate etc then needed to load remote image using XMLHTTPRequest

--- window.js
+++ window.js
@@ -6,6 +6,8 @@ console.log("script loaded");
 
 var PlantUml = {
 
+    currentFileEntry:null,
+
     /**
      * HTML ELEMENT References
      */
@@ -19,16 +21,27 @@ var PlantUml = {
      * Initialise stuff
      */
     init:function init(){
+        PlantUml.buttonNew          = document.querySelector("#buttonNew");
         PlantUml.buttonOpen         = document.querySelector("#buttonOpen");
         PlantUml.buttonSave         = document.querySelector("#buttonSave");
+        PlantUml.buttonSaveAs       = document.querySelector("#buttonSaveAs");
+        PlantUml.buttonGenerate     = document.querySelector("#buttonGenerate");
+
         PlantUml.outputTextFileInfo = document.querySelector("#inputFileInfo");
         PlantUml.textarea           = document.querySelector("#editor");
         PlantUml.logTextarea        = document.querySelector("#log");
 
+        PlantUml.buttonNew.addEventListener("click",PlantUml.file.new);
         PlantUml.buttonOpen.addEventListener("click",PlantUml.file.choose);
         PlantUml.buttonSave.addEventListener("click",PlantUml.file.save);
+        PlantUml.buttonSaveAs.addEventListener("click",PlantUml.file.saveAs);
+        PlantUml.buttonGenerate.addEventListener("click",PlantUml.generate);
 
         PlantUml.log("loaded");
+    },
+
+    generate:function generate(){
+        refreshDiagram("diagram",PlantUml.textarea.value);
     },
 
     /**
@@ -37,52 +50,41 @@ var PlantUml = {
     file:{
         accepts: [{
             mimeTypes: ['text/*'],
-            extensions: ['js', 'css', 'txt', 'html', 'xml', 'tsv', 'csv', 'rtf']
+            extensions: ['txt','seq']
         }],
 
-        choose:function load(){
-            console.log("choosing file");
-            var config = {type: 'openFile', accepts: PlantUml.file.accepts}
-            chrome.fileSystem.chooseEntry(config, function(theEntry) {
-                if (!theEntry) {
-                    PlantUml.log('No file selected');
-                    return;
-                }
-                // use local storage to retain access to this file
-                chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(theEntry)});
-                PlantUml.file.load(theEntry);
-            });
-        },
-        load:function loadFile(fileEntry){
-            console.log("LoadFile",fileEntry);
-            fileEntry.file(function(file) {
-                PlantUml.file.readAsText(fileEntry, function(result) {
-                    PlantUml.textarea.value = result;
+        choose:function choose(){
+            console.log("Choose file");
+            try{
+                var config = {type: 'openFile', accepts: PlantUml.file.accepts}
+                chrome.fileSystem.chooseEntry(config, function(choosen){
+                    !!choosen && PlantUml.file.load(choosen);
                 });
-                //saveFileButton.disabled = false; // allow the user to save the content
-                PlantUml.file.showInfo(fileEntry);
-            });
+            }catch(e){
+                console.log(e);
+            }
         },
-        /**
-         * Read file contents as Text
-         * @param fileEntry
-         * @param callback called with file contents
-         */
-        readAsText:function readAsText(fileEntry, callback) {
-            fileEntry.file(function(file) {
+        load:function load(fileEntry){
+            console.log("LoadFile",fileEntry);
+            fileEntry.file(function(file){
                 var reader = new FileReader();
                 reader.onerror = function(e){console.error(e)};
                 reader.onload = function(e) {
-                    callback(e.target.result);
+                    PlantUml.textarea.value = e.target.result;
                 };
                 reader.readAsText(file);
             });
+            // use local storage to retain access to this file
+            chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(fileEntry)});
+            PlantUml.currentFileEntry = fileEntry;
+            // update display with file info
+            PlantUml.file.showInfo(fileEntry);
         },
         /**
          * Display File Info
          * @param fileEntry
          */
-        showInfo:function showFileInfo(fileEntry){
+        showInfo:function showInfo(fileEntry){
             if (fileEntry.isFile) {
                 chrome.fileSystem.getDisplayPath(fileEntry, function(path) {
                     PlantUml.log("File path:",path);
@@ -97,19 +99,36 @@ var PlantUml = {
                 document.querySelector('#file_size').textContent = "N/A";
             }
         },
+
         /**
-         * Save contents to File
+         * Save contents to currently selected file
          */
         save:function save(){
-            console.log("save");
-            var config = {type: 'saveFile', suggestedName: chosenEntry.name};
-            chrome.fileSystem.chooseEntry(config, function(writableEntry) {
-                var blob = new Blob([textarea.value], {type: 'text/plain'});
-                writeFileEntry(writableEntry, blob, function(e) {
-                    output.textContent = 'Write complete :)';
-                });
+            console.log("save..");
+            if(PlantUml.currentFileEntry){
+                chrome.fileSystem.getWritableEntry(PlantUml.currentFileEntry,PlantUml.file.writeToFile);
+            }else{
+                console.error("SAVE CALLED IN ERROR: no file allocated")
+            }
+        },
+
+        /**
+         * Save contents As.. to File
+         */
+        saveAs:function saveAs(){
+            console.log("save as..");
+            var config = {type: 'saveFile', suggestedName: PlantUml.currentFileEntry.name};
+            chrome.fileSystem.chooseEntry(config, PlantUml.file.writeToFile);
+        },
+
+        writeToFile:function(writableEntry){
+            var contents = PlantUml.textarea.value;
+            var blob = new Blob([contents], {type: 'text/plain'});
+            FileSystem.writeFileEntry(writableEntry, blob, function(e) {
+                console.log('Write complete :)',writableEntry.name);
             });
         }
+
     },
 
     /**
@@ -120,6 +139,78 @@ var PlantUml = {
             PlantUml.logTextarea.value = PlantUml.logTextarea.value + "\n" + m.toString();
             // todo: scroll to bottom
         });
+    }
+}
+
+/**
+ *
+ * Wrapper for CHROME filesystem access methods from GOOGLE
+ *
+ */
+var FileSystem = {
+    /**
+     * Write to a file
+     *
+     * @param writableEntry
+     * @param opt_blob
+     * @param callback
+     */
+    writeFileEntry:function writeFileEntry(writableEntry, opt_blob, callback) {
+        if (!writableEntry) {
+            output.textContent = 'Nothing selected.';
+            return;
+        }
+
+        writableEntry.createWriter(function(writer) {
+
+            writer.onerror = function(e){console.error(e);};
+            writer.onwriteend = callback;
+
+            // If we have data, write it to the file. Otherwise, just use the file we
+            // loaded.
+            if (opt_blob) {
+                writer.truncate(opt_blob.size);
+                FileSystem.waitForIO(writer, function() {
+                    writer.seek(0);
+                    writer.write(opt_blob);
+                });
+            } else {
+                PlantUml.currentFileEntry.file(function(file) {
+                    writer.truncate(file.fileSize);
+                    waitForIO(writer, function() {
+                        writer.seek(0);
+                        writer.write(file);
+                    });
+                });
+            }
+        }, function(e){console.error(e);});
+    },
+
+    /**
+     * Callback for IO
+     *
+     * @param writer
+     * @param callback
+     */
+    waitForIO:function waitForIO(writer, callback) {
+        // set a watchdog to avoid eventual locking:
+        var start = Date.now();
+        // wait for a few seconds
+        var reentrant = function() {
+            if (writer.readyState===writer.WRITING && Date.now()-start<4000) {
+                setTimeout(reentrant, 100);
+                return;
+            }
+            if (writer.readyState===writer.WRITING) {
+                console.error("Write operation taking too long, aborting!"+
+                    " (current writer readyState is "+writer.readyState+")");
+                writer.abort();
+            }
+            else {
+                callback();
+            }
+        };
+        setTimeout(reentrant, 100);
     }
 }
 
