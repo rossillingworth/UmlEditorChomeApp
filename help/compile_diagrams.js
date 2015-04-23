c0465aeb5173541c4eff3516cc1ba17b90a317a6
rossillingworth <ross_illingworth@hotmail.com>
2015-04-23 23:48:49 +0100

    initial help compiler

--- help/compile_diagrams.js
+++ help/compile_diagrams.js
@@ -8,3 +8,23 @@
 // send each one to PlantUML server
 // save response in file /help/images/xxx.png
 
+var fs = require("fs");
+
+function getFiles (dir, files_){
+    files_ = files_ || [];
+    var files = fs.readdirSync(dir);
+    for (var i in files){
+        var name = dir + '/' + files[i];
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, files_);
+        } else {
+            files_.push(name);
+        }
+    }
+    return files_;
+}
+
+var files = getFiles('.');
+files = files.filter(function(f){return f.split("").reverse().join("").indexOf("elmu.")==0});
+
+console.log(files);
