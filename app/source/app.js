faf08996f57d56b136c53114f99cbe2dc0c6b1dc
Ross Illingworth <RossL@devinfotech.co.uk>
2015-05-15 15:32:59 +0100

    enyo ui coming on

--- app/source/app.js
+++ app/source/app.js
@@ -9,11 +9,86 @@ enyo.kind({
     name: "UMLEditor",
     kind: "FittableRows",
     fit: true,
+    classes:"onyx",
     components:[
-        {kind: "onyx.Toolbar", content: "Hello World"},
-        {kind: "enyo.Scroller", fit: true, components: [
-            {name: "main", classes: "nice-padding", allowHtml: true}
+        {kind: "onyx.Toolbar", components:[
+
+            {kind: "onyx.MenuDecorator", components: [
+                {content: "File"},
+                {kind: "onyx.Tooltip", content: "Tap to open..."},
+                {kind: "onyx.Menu", scrolling:false, components: [
+                    {content: "New"},
+                    {kind:"onyx.Submenu",content:"New",components:[
+                        {content:"Action"},
+                        {content:"Sequence"}
+                    ]},
+                    {components:[
+                        {kind: "onyx.MenuDecorator", components: [
+                            {content: "New...>"},
+                            {kind: "onyx.Tooltip", content: "Tap to open..."},
+                            {kind: "onyx.Menu", components: [
+                                {content: "PlantUML"},
+                                {content: "About"}
+                            ]}
+                        ]}
+                    ]},
+                    {content: "Open"},
+                    {classes: "onyx-menu-divider"},
+                    {content: "Save"},
+                    {content: "Save As"},
+                    {classes: "onyx-menu-divider"},
+                    {content: "Print"}
+                ]}
+            ]},
+
+            {kind: "onyx.MenuDecorator", components: [
+                {content: "View"},
+                {kind: "onyx.Tooltip", content: "Tap to open..."},
+                {kind: "onyx.Menu", components: [
+                    {components:[
+                        {content: "Refresh"},
+                        {kind:"onyx.ToggleButton",content:"Refresh",value:true}
+                    ]},
+                    {classes: "onyx-menu-divider"},
+                    {content: "Horizontal"},
+                    {content: "Vertical"}
+                ]}
+            ]},
+
+            {kind: "onyx.MenuDecorator", components: [
+                {content: "Help"},
+                {kind: "onyx.Tooltip", content: "Tap to open..."},
+                {kind: "onyx.Menu", components: [
+                    {content: "PlantUML"},
+                    {content: "About"}
+                ]}
+            ]}
+
+
         ]},
+
+        // TODO: add helpers on LHS
+
+//        {kind:"FittableColumns",fit:true,components:[
+//            {kind: "enyo.Scroller", fit: false, components: [
+//                {content:"x"},
+//                {content:"y"}
+//            ]},
+//            {kind: "enyo.Scroller", fit: true, components: [
+//
+//            ]},
+//        ]},
+
+        {kind:"Panels",fit:true,arrangerKind:"CarouselArranger",components:[
+            {content:"Editor",style:"width:50%;background-color:green;"},
+            {content:"[]",style:"background-color:gray;"},
+            {content:"Image",style:"background-color:yellow;"}
+        ]},
+
+//        {kind: "enyo.Scroller", fit: true, components: [
+//            {name: "main", classes: "nice-padding", allowHtml: true}
+
+//        ]},
         {kind: "onyx.Toolbar", components: [
             {kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
         ]}
